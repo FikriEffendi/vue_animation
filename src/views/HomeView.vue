d1cc2f2883859a9646d88e056f998b9bafbdfe1d
aerochops <150325960+aerochops@users.noreply.github.com>
2024-07-30 17:22:24 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue';
+
+
+const showText=ref('true');
+</script>
+
+<template>
+  <main>
+    <transition name="fade">
+      <h1 v-if="showText">Halo banh!</h1>
+    </transition>
+    <button @click="showText=!showText">toggle</button>
+  </main>
+</template>
+
+<style scoped>
+.fade-enter-from,
+.fade-leave-to{
+  @apply opacity-0 ;
+}
+
+.fade-enter-to,
+.fade-leave-from{
+  @apply opacity-100;
+}
+
+.fade-enter-active{
+  @apply transition-all duration-1000 ease-in; 
+}
+
+.fade-leave-active{
+  @apply transition-all duration-1000 ease-out;
+}
+</style>
