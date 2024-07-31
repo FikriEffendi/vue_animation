b5af0ce8988e5a7a26c55119bfff5240168ca80f
aerochops <150325960+aerochops@users.noreply.github.com>
2024-07-31 13:40:59 +0700

    second commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,34 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
 
-
-const showText=ref('true');
+const showText = ref("true");
 </script>
 
 <template>
   <main>
-    <transition name="fade">
-      <h1 v-if="showText">Halo banh!</h1>
-    </transition>
-    <button @click="showText=!showText">toggle</button>
+    <div class="relative">
+      <transition name="fade">
+        <h1 v-if="showText" key="text-1" class="absolute">Halo banh!</h1>
+        <h1 v-else key="text-2" class="absolute">Oke</h1>
+      </transition>
+      <button @click="showText = !showText" class="mt-3">toggle</button>
+    </div>
   </main>
 </template>
 
 <style scoped>
 .fade-enter-from,
-.fade-leave-to{
-  @apply opacity-0 ;
+.fade-leave-to {
+  @apply opacity-0;
 }
 
 .fade-enter-to,
-.fade-leave-from{
+.fade-leave-from {
   @apply opacity-100;
 }
 
-.fade-enter-active{
-  @apply transition-all duration-1000 ease-in; 
-}
-
-.fade-leave-active{
-  @apply transition-all duration-1000 ease-out;
+.fade-enter-active,
+.fade-leave-active {
+  @apply transition-all duration-1000 ease-in-out;
 }
 </style>
