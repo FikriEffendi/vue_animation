189d34656324f54f98ad85ac36d73371e0e54bd4
aerochops <150325960+aerochops@users.noreply.github.com>
2024-08-14 16:07:23 +0700

    menambahkan router

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,9 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
+  <header class="container">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </header>
   <RouterView />
 </template>
